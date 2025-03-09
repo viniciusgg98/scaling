5292a72ac77e292a2e3883b54487e75f2fc8e9c6
Vinícius Ghilardi <viniciusgg98@gmail.com>
2025-03-09 17:30:11 -0300

    configuração inicial
    Configuração inicial de uma api usando o FASTAPI

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/testei")
+async def funcaoteste():
+    return {"teste": "deu certo"}
